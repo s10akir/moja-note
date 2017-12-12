7c8c534ee2b90a0ba2cb54956a1629c6eab629ac
Akira Shinohara <34061817+s10akir@users.noreply.github.com>
2017-12-12 22:28:01 +0900

    [new] dbに沿った起動時処理/表示処理を追加)

--- app/script/index.js
+++ app/script/index.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const Datasotre = require('nedb');
+let db = new Datasotre({
+    filename: 'app/db/notes.db',
+    autoload: true
+});
+
+db.find('', function (err, docs) {
+    docs.forEach(function (element) {
+        console.log(element['title']);
+        let dom = '<div class="note" id="'+ element['_id'] + '" onclick="preview(' + element['_id'] +');"><p>' + element['title'] + '</p></div>';
+        document.getElementById('notes-search').insertAdjacentHTML('afterend', dom) ;
+    });
+});
+
+function preview(id) {
+    db.find(id, function (err, docs) {
+        console.log(docs);
+        let dom = '<h1>' + docs[0]['title'] + '</h1>' +
+                  '<p>'+ docs[0]['text'] + '</p>';
+        document.getElementById('note-view-body').innerHTML = dom;
+    })
+}

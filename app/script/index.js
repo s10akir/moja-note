4d9ae1d7e8ba79a83ee2e29dca596fb180235d2e
Akira Shinohara <34061817+s10akir@users.noreply.github.com>
2017-12-12 23:00:56 +0900

    [change] 体裁を変更

--- app/script/index.js
+++ app/script/index.js
@@ -9,8 +9,8 @@ let db = new Datastore({
 db.find('', function (err, docs) {
     docs.forEach(function (element) {
         console.log(element['title']);
-        let dom = '<div class="note" id="'+ element['_id'] + '" onclick="preview(' + element['_id'] +');"><p>' + element['title'] + '</p></div>';
-        document.getElementById('notes-search').insertAdjacentHTML('afterend', dom) ;
+        let dom = '<div class="note" id="'+ element['_id'] + '" onclick="preview(' + element['_id'] +');">' + element['title'] + '<div class="note-tag">' + element['tag'] + '</div></div>';
+        document.getElementById('notes-search').insertAdjacentHTML('afterend', dom);
     });
 });
 
@@ -20,8 +20,7 @@ const marked = require('marked');
 
 function preview(id) {
     db.find(id, function (err, docs) {
-        console.log(docs);
-        let dom = '<h1>' + docs[0]['title'] + '</h1>' + marked(docs[0]['text']);
+        let dom = '<note-title>' + docs[0]['title'] + '</note-title>' + marked(docs[0]['text']);
         document.getElementById('note-view-body').innerHTML = dom;
     })
 }

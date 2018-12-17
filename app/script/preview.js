d2ec2fa19ed37c3937144130de180224e116d762
Akira Shinohara <k017c1067@it-neec.jp>
2018-12-18 00:38:00 +0900

    [new] 印刷時任意の位置で改ページできるように +++ で改ページ

--- app/script/preview.js
+++ app/script/preview.js
@@ -5,6 +5,10 @@ let marked = require('marked');
 
 
 ipcRenderer.on('text-update', (event, text) => {
+    // +++をページ区切りとして解釈する
+    text = '+++\n' + text;  // 頭のdivが必要
+    text = text.replace(/(^|\n)\+\+\+(\n|$)/g, '</div><div class="page">');  // TODO: ローテクすぎるのでなんとかしたい
+
     let dom = marked(text);
     let preview = document.getElementById('preview-text');
 
